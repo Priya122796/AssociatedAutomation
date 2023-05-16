18e341f20073ae777d3f3203ba18455ccbbc3626
Priya122796 <67692697+Priya122796@users.noreply.github.com>
2023-05-16 19:29:24 +0530

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  projectId: 'yb6s1t',
+  fixturesFolder: false,
+  video: false,
+  
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+    },
+  },
+})
+
+require('@applitools/eyes-cypress')(module)
